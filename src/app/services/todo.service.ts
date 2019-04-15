636bb72efa070b65172e7df7cd86fb35a4a4687b
dagmarawieczorek <dagmara.sylwia.w@gmail.com>
2019-04-15 21:22:16 +0200

    created service, toggling and deleting todos

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,29 +1,34 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Todo } from '../models/Todo';
+import { Observable } from 'rxjs';
 
+const httpOptions ={
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+}
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
+  todosUrl:string = 'https://jsonplaceholder.typicode.com/todos';
+  todosLimit='?_limit=30';
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
-  getTodos(){
-    return [
-      {
-        id:1,
-        title: 'Todo One',
-        completed: false
-      },
-      {
-        id:2,
-        title: 'Todo 2',
-        completed: true
-      },
-      {
-        id:3,
-        title: 'Todo 3',
-        completed: false
-      },
-    ]
+  getTodos():Observable<Todo[]>{
+    return this.http.get<Todo[]>(`${this.todosUrl}${this.todosLimit}`);
+  }
+
+  toggleCompleted(todo: Todo):Observable<any>{
+    const url= `${this.todosUrl}/${todo.id}`
+    return this.http.put(url, todo, httpOptions );
+
+  }
+
+  deleteTodo(todo: Todo){
+    const url= `${this.todosUrl}/${todo.id}`
+    return this.http.delete(url)
   }
 }
